c4c098deeb65c36a2fed0b19294ad7d0f36e4114
Naoki Yoshimaru <nydsa529422@gmail.com>
2022-09-01 16:39:22 +0900

    create morganFP Data

--- copy.sh
+++ copy.sh
@@ -1,2 +1,6 @@
 scp /Users/naokiyoshimaru/Desktop/pro-FingerPrint-AE/data/hidden_vec_fingreprint_8212.pickle miluser@192.168.100.67:/home/miluser/yoshimaru_env/FlavorGraph/src8298/input
 scp miluser@192.168.100.67:/home/miluser/yoshimaru_env/FlavorGraph/src_Metapath2vec+NPMI_8298/input/node2fp_8212.pickle /Users/naokiyoshimaru/Desktop/pro-FingerPrint-AE/data
+
+
+scp /Users/naokiyoshimaru/Desktop/FingerPrint-Consideration/data/node2fp_word2vec_8212.pickle miluser@192.168.100.66:/home/miluser/yoshimaru_citrine_backup/yoshimaru_env/FlavorGraph/src_Metapath2vec+M2V
+scp /Users/naokiyoshimaru/Desktop/FingerPrint-Consideration/data/Morgan2binary_8212.pickle miluser@192.168.100.66:/home/miluser/yoshimaru_citrine_backup/yoshimaru_env/FlavorGraph/src_Metapath2vec+M2V
